2275bbbdfaee0114f48a0e1eb35ac024e58d2c7d
matheu55391 <matheus.manulo55@gmail.com>
2020-09-30 16:04:13 -0300

    langueges suport

--- tts/tts.py
+++ tts/tts.py
@@ -2,9 +2,16 @@
 # Autor: Meguinha  ( Matheus Felipe Vieira Santiago )
 # PYTHON 3.7.6
 # gtts, os
-from gtts import gTTS as mytts
+from gtts import gTTS
+import os
 
-def tts_convert(text:str, file_name: str="default", play:bool =False):
+def tts_config(text:str, file_name: str="default", set_lang: str = "en"):
     #creates an mp3 file using a string 
-    file = mytts(text)
-    file.save(f"{file_name}.mp3")
+    file_name = f'{file_name}.mp3'
+    file = gTTS(text, lang= set_lang)
+    file.save(file_name)
+    return file_name
+
+
+#test
+#tts("Isso é apenas um teste :) ", file_name="bruninhars", set_lang= "pt")
